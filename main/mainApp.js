f9af0ae3a04349d7c79b74e0da8c6a71849ef113
Jan20 <jan-philip.schumann@gmx.de>
2015-06-12 15:49:31 +0200

    Eyetracking 0.1

--- main/mainApp.js
+++ main/mainApp.js
@@ -1,12 +1,28 @@
 var mainApp = angular.module("mainApp", ['ngRoute']);
 
-mainApp.config([function () {
-	
-}])
+mainApp.config(function ($routeProvider) {
+	$routeProvider.when('/',{
+		templateUrl: 'views/login.html',
+		// controller: 'loginController'
+	});	
 
+	$routeProvider.when('/step/1',{
+		templateUrl: 'views/showFirstTime.html',
+		controller: 'mainController'
+	});	
 
-mainApp.controller("mainController", ['$scope', function($scope) {
-   $scope.name = "Carlos";
+	$routeProvider.when('/step/2',{
+		templateUrl: 'views/chooseFirstTime.html',
+		controller: 'mainController'
+	});	
 
-}]);
+	$routeProvider.when('/step/3',{
+		templateUrl: 'views/showSecondTime.html',
+		controller: 'mainController'
+	});
 
+	$routeProvider.when('/step/4',{
+		templateUrl: 'views/chooseSecondTime.html',
+		controller: 'mainController'
+	});
+});
---
+++ main/mainController.js
@@ -0,0 +1,103 @@
+mainApp.controller("mainController", ['$scope', function($scope) {
+   $scope.name = "Carlos";
+
+   $scope.products = [
+
+   {
+   		id: 1,
+   		image: 'http://ecx.images-amazon.com/images/I/71h9sZZfmmL._SL1366_.jpg',
+   		url: 'http://www.amazon.de/LG-40UB800V-LED-Backlight-Fernseher-Ultra-Remote/dp/B00NIIS8L4/ref=sr_1_1?ie=UTF8&qid=1434113539&sr=8-1&keywords=4k+fernseher',
+   		visitedFirstTime: false,
+		choosedFirstTime: false,
+		visitedSecondTime: false,
+		choosedSecondTime: false
+   },
+
+   {
+   		id: 2,
+   		image: '',
+   		url: 'http://www.amazon.de/LG-40UB800V-LED-Backlight-Fernseher-Ultra-Remote/dp/B00NIIS8L4/ref=sr_1_1?ie=UTF8&qid=1434113539&sr=8-1&keywords=4k+fernseher',
+   		visitedFirstTime: false,
+		choosedFirstTime: false,
+		visitedSecondTime: false,
+		choosedSecondTime: false
+   },
+
+   {
+   		id: 3,
+   		image: '',
+   		url: 'http://www.amazon.de/LG-40UB800V-LED-Backlight-Fernseher-Ultra-Remote/dp/B00NIIS8L4/ref=sr_1_1?ie=UTF8&qid=1434113539&sr=8-1&keywords=4k+fernseher',
+   		visitedFirstTime: false,
+		choosedFirstTime: false,
+		visitedSecondTime: false,
+		choosedSecondTime: false
+   },
+
+   {
+   		id: 4,
+   		image: '',
+   		url: 'http://www.amazon.de/LG-40UB800V-LED-Backlight-Fernseher-Ultra-Remote/dp/B00NIIS8L4/ref=sr_1_1?ie=UTF8&qid=1434113539&sr=8-1&keywords=4k+fernseher',
+   		visitedFirstTime: false,
+		choosedFirstTime: false,
+		visitedSecondTime: false,
+		choosedSecondTime: false
+   },
+
+   {
+   		id: 5,
+   		image: '',
+   		url: 'http://www.amazon.de/LG-40UB800V-LED-Backlight-Fernseher-Ultra-Remote/dp/B00NIIS8L4/ref=sr_1_1?ie=UTF8&qid=1434113539&sr=8-1&keywords=4k+fernseher',
+   		visitedFirstTime: false,
+		choosedFirstTime: false,
+		visitedSecondTime: false,
+		choosedSecondTime: false
+   },
+      
+   {
+   		id: 6,
+   		image: '',
+   		url: 'http://www.amazon.de/LG-40UB800V-LED-Backlight-Fernseher-Ultra-Remote/dp/B00NIIS8L4/ref=sr_1_1?ie=UTF8&qid=1434113539&sr=8-1&keywords=4k+fernseher',
+   		visitedFirstTime: false,
+		choosedFirstTime: false,
+		visitedSecondTime: false,
+		choosedSecondTime: false
+   },
+      
+   {
+   		id: 7,
+   		image: '',
+   		url: 'http://www.amazon.de/LG-40UB800V-LED-Backlight-Fernseher-Ultra-Remote/dp/B00NIIS8L4/ref=sr_1_1?ie=UTF8&qid=1434113539&sr=8-1&keywords=4k+fernseher',
+   		visitedFirstTime: false,
+		choosedFirstTime: false,
+		visitedSecondTime: false,
+		choosedSecondTime: false
+   },
+      
+   {
+   		id: 8,
+   		image: '',
+   		url: 'http://www.amazon.de/LG-40UB800V-LED-Backlight-Fernseher-Ultra-Remote/dp/B00NIIS8L4/ref=sr_1_1?ie=UTF8&qid=1434113539&sr=8-1&keywords=4k+fernseher',
+   		visitedFirstTime: false,
+		choosedFirstTime: false,
+		visitedSecondTime: false,
+		choosedSecondTime: false
+   },
+      
+   {
+   		id: 9,
+   		image: '',
+   		url: 'http://www.amazon.de/LG-40UB800V-LED-Backlight-Fernseher-Ultra-Remote/dp/B00NIIS8L4/ref=sr_1_1?ie=UTF8&qid=1434113539&sr=8-1&keywords=4k+fernseher',
+   		visitedFirstTime: false,
+		choosedFirstTime: false,
+		visitedSecondTime: false,
+		choosedSecondTime: false
+   }
+
+];
+
+
+
+
+
+}]);
+
