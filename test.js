0a9ae4191fa6e45eb0fb951e14903b186e975df5
Jan20 <jan-philip.schumann@gmx.de>
2015-07-20 17:08:59 +0200

    9.1.1

--- test.js
+++
@@ -1,7 +0,0 @@
-var data = [["Minsk",100000], ["Riga",200000]];
-
-window.exportData = function exportData() {
-    alasql("SELECT * INTO CSV('test.csv',{headers:true}) FROM ?",[data]);
-    //alasql('SELECT * INTO CSV("test.csv". {headers:true}) FROM ?',[data]);
-}
-
