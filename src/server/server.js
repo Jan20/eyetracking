aac5be32e37160601633a7c3bab28d8405412a4a
Jan20 <jan-philip.schumann@gmx.de>
2015-09-30 14:53:19 +0200

    Version 2.2.1
    test

--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,101 @@ var http = require('http');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://Jan20:0staticVoid0@ds041992.mongolab.com:41992/leaf');
 
+
+
+experimentRoute
+	.route('/experiments')
+		.post(postExperiment)
+		.get(getExperiments);
+
+
+experimentRoute
+	.route('/experiment/:experimentId')
+		.get(getExperiment)
+		.put(updateExperiment);
+
+
+/**
+*
+*	Die folgende Funktion implementiert die GET ALL Funktionalität der Route
+*
+*/
+function getExperiments(req, res){
+	Experiment.find({}, function(err, experiments){
+		if(err)
+			res.send('Bei der Abfrage aller Experimente ist ein Fehler aufgetreten: ' + err);
+		res.json(experiments);
+		
+	});
+}
+
+/**
+*
+*	Die folgende Funktion implementiert die GET ONE Funktionalität der Route
+*
+*/
+function getExperiment(req, res){
+	Experiment.findOne({experimentId: req.params.experimentId}, function(err, experiment){
+		if(err)
+			res.send('Bei der Abfrage eines Experiments ist ein Fehler aufgetreten: ' + err);
+		res.json(experiment);
+	});
+}
+
+
+/**
+*
+*	Die folgende Funktion implementiert die POST Funktionalität der Route
+*
+*/
+function postExperiment(req, res){
+	var experiment = new Experiment({
+		experimentId: req.body.experimentId,
+		description: req.body.description,
+		buttonActive: req.body.buttonActive,
+		buttonInactive: req.body.buttonInactive
+	});
+	experiment.save(function(err, a){
+		if(err)
+			res.send('Bei der Abfrage eines Experiments ist ein Fehler aufgetreten: ' + err);
+		res.json(experiment);
+	});
+}
+
+
+/**
+*
+*	Die folgende Funktion implementiert die GET ALL Funktionalität der Route
+*
+*/
+function updateExperiment(req, res){
+	Experiment.findOne({experimentId: req.params.experimentId}, function(err, experiment){
+		experiment.experimentId = req.body.experimentId;
+		experiment.description = req.body.description;
+		experiment.buttonActive = req.body.buttonActive;
+		experiment.buttonInactive = req.body.buttonInactive;
+		experiment.save(function(err){
+		if(err)
+			res.send('Bei der Änderung eines Experiments ist ein Fehler aufgetreten: ' + err);
+		res.json(experiment);
+		});
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.use('/asdf', function(req, res){
 	res.send('Hello');
 });
@@ -27,7 +122,7 @@ app.use('/api', userRoute);
 var productRoute = require('./api/productRoute');
 app.use('/api', productRoute);
 
-var experimentRoute = require('./api/experimentRoute');
+// var experimentRoute = require('./api/experimentRoute');
 app.use('/api', experimentRoute);
 
 var questionRoute = require('./api/questionRoute');
