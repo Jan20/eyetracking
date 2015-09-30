8e8276993db24219085d347839ba8652f5d928ee
Jan20 <jan-philip.schumann@gmx.de>
2015-09-30 15:05:32 +0200

    Version 2.2.4
    require js 2

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,4 @@
+var requirejs = require('requirejs');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -7,106 +8,6 @@ var http = require('http');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://Jan20:0staticVoid0@ds041992.mongolab.com:41992/leaf');
-
-var experimentRoute = express.Router();
-
-
-experimentRoute
-	.route('/experiments')
-		.post(postExperiment)
-		.get(getExperiments);
-
-
-experimentRoute
-	.route('/experiment/:experimentId')
-		.get(getExperiment)
-		.put(updateExperiment);
-
-
-/**
-*
-*	Die folgende Funktion implementiert die GET ALL Funktionalität der Route
-*
-*/
-function getExperiments(req, res){
-	Experiment.find({}, function(err, experiments){
-		if(err)
-			res.send('Bei der Abfrage aller Experimente ist ein Fehler aufgetreten: ' + err);
-		res.json(experiments);
-		
-	});
-}
-
-/**
-*
-*	Die folgende Funktion implementiert die GET ONE Funktionalität der Route
-*
-*/
-function getExperiment(req, res){
-	Experiment.findOne({experimentId: req.params.experimentId}, function(err, experiment){
-		if(err)
-			res.send('Bei der Abfrage eines Experiments ist ein Fehler aufgetreten: ' + err);
-		res.json(experiment);
-	});
-}
-
-
-/**
-*
-*	Die folgende Funktion implementiert die POST Funktionalität der Route
-*
-*/
-function postExperiment(req, res){
-	var experiment = new Experiment({
-		experimentId: req.body.experimentId,
-		description: req.body.description,
-		buttonActive: req.body.buttonActive,
-		buttonInactive: req.body.buttonInactive
-	});
-	experiment.save(function(err, a){
-		if(err)
-			res.send('Bei der Abfrage eines Experiments ist ein Fehler aufgetreten: ' + err);
-		res.json(experiment);
-	});
-}
-
-
-/**
-*
-*	Die folgende Funktion implementiert die GET ALL Funktionalität der Route
-*
-*/
-function updateExperiment(req, res){
-	Experiment.findOne({experimentId: req.params.experimentId}, function(err, experiment){
-		experiment.experimentId = req.body.experimentId;
-		experiment.description = req.body.description;
-		experiment.buttonActive = req.body.buttonActive;
-		experiment.buttonInactive = req.body.buttonInactive;
-		experiment.save(function(err){
-		if(err)
-			res.send('Bei der Änderung eines Experiments ist ein Fehler aufgetreten: ' + err);
-		res.json(experiment);
-		});
-	});
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.use('/asdf', function(req, res){
-	res.send('Hello');
-});
 
 
 app.set('port', process.env.PORT || 3000);
@@ -123,7 +24,7 @@ app.use('/api', userRoute);
 var productRoute = require('./api/productRoute');
 app.use('/api', productRoute);
 
-// var experimentRoute = require('./api/experimentRoute');
+var experimentRoute = require('./api/experimentRoute');
 app.use('/api', experimentRoute);
 
 var questionRoute = require('./api/questionRoute');
